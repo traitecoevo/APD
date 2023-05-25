a70fef455b1f0a2a3f2cfdb6bde155b35ccc4244
Daniel Falster <daniel.falster@unsw.edu.au>
2023-05-25 16:57:05 +1000

    Reorganise Build
    - add master build control
    - change identifier for groups from traits to trait_group
    - embed scripts into a function
    - remove old methods
    - rename outputs to APD

--- R/kable.R
+++ R/kable.R
@@ -4,28 +4,31 @@
 #' @param ... arguments passed to `kableExtra::kable()`
 #' @importFrom rlang .data
 #' @export
-util_kable_styling_html <- function(...) {
-    txt <- 
-      kableExtra::kable(...) %>%
-      kableExtra::kable_styling(..., 
-                  bootstrap_options = c("striped", "hover", "condensed", "responsive"), 
-                  full_width = FALSE, 
-                  position = "left"
-                  ) 
-    
-    # hack to add margin to plot
-    gsub('style="width: auto ', 'style="margin-left:30px; width: auto ', txt)
+print_table_html <- function(...) {
+  gt(...) %>%
+    cols_align(align = "left") %>%
+    cols_width(
+      name ~ px(150),
+      description ~ px(600)
+    ) %>%
+    cols_label(
+      name = "**Property**",
+      description = "**Value**"
+    ) %>%
+    print()
 }
 
 
 #' @export
-util_kable_styling_pdf <- function(...) {
+print_table_pdf <- function(...) {
     kableExtra::kbl(format = "latex", booktabs = T, linesep = "\\addlinespace", longtable = T, ...) %>%
     kableExtra::kable_styling(latex_options = c("striped", "HOLD_position")) %>%
-    kableExtra::column_spec(2, width = "32em")
+    kableExtra::column_spec(2, width = "32em") %>%
+    print()
 }
 
 #' @export
-util_kable_styling_docx <- function(...) {
-  kableExtra::kbl(...)
+print_table_docx <- function(...) {
+  kableExtra::kbl(...) %>%
+  print()
 }
